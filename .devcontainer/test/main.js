44648a27fed13e6cbdc4eb04c6988251ffb67dc3
legacy1979yy <82152030+legacy1979yy@users.noreply.github.com>
2023-07-03 11:04:30 +0000

    xyz

---
+++ .devcontainer/test/main.js
@@ -0,0 +1,7 @@
+// 四則演算を行う
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+
+
--- sample/chart/app/astro.config.mjs
+++ sample/chart/app/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dzeyelid.github.io',
+  site: 'https://legacy1979yy.github.io',
   base: '/github-copilot-playground'
 });
